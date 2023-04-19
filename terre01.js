5a3afc0bfd7136930f4d53f9a7e3f5d1c5268c34
Hawo War <warhawo@gmail.com>
2023-04-19 14:38:47 +0000

    exerce 01 finir

---
+++ terre01.js
@@ -0,0 +1,16 @@
+/* Un programme qui affiche son nom de fichier */
+
+function printName() {
+  let result = "" ; 
+  const myArgv = process.argv.slice(1);
+  let title = myArgv[0].split('/');
+  
+  result = title[title.length - 1]; 
+
+  return result;
+}
+
+
+console.log(process.argv.slice(0));
+
+/* process.argv.slice(1) retourn un table contenant les aguments tab[0] chemin du fichier tab[1] = premier argument */
