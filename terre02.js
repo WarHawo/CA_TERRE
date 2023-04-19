ce5af8028876a7caa4a2099d8597844608d92cf1
Hawo War <warhawo@gmail.com>
2023-04-19 15:23:15 +0000

    exercice finit et compris

---
+++ terre02.js
@@ -0,0 +1,10 @@
+/* un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+*/
+function printArg () {
+  const myArgv = process.argv.slice(2);
+  for (let i = 0 ; i < myArgv.length ; i++) {
+    console.log(myArgv[i]);
+  }
+}
+
+printArg();
