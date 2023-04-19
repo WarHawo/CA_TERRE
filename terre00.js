9908959283102b39804b3cba13bbf3d1c77f5b39
Hawo War <warhawo@gmail.com>
2023-04-19 13:01:45 +0000

    alphabet exercice done

---
+++ terre00.js
@@ -0,0 +1,14 @@
+/* Programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne. 
+** J'utilise le code ASCII des carctere 
+*/
+
+function alphabet() {
+  let abc = [];
+
+  for (let i = 97; i < 122 ; i++) {
+    abc += String.fromCharCode(i);
+  }
+  return abc;
+}
+
+console.log(alphabet())
