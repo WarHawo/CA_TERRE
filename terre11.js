f8234de9a10974edaa86008a8af67bf6685db734
War Hawo <warhawo@gmail.com>
2023-04-20 16:44:20 +0000

    idee resolution exercice 11

---
+++ terre11.js
@@ -0,0 +1,3 @@
+/* Programme qui transforme une heure affichée en format 24h en une heure affichée en format 12h. */
+
+/* if heur suppérieur à 12 faire heure -12 et rajouter PM sinon juste rajouter AM*/
