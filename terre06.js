abb99ac19d2c3005479adf4f39e197688b333432
Hawo War <warhawo@gmail.com>
2023-04-20 15:08:49 +0000

    correction overflow

--- terre06.js
+++ terre06.js
@@ -1,12 +1,12 @@
 /* Programme qui affiche l’inverse de la chaîne de caractères donnée en argument. */
 
-function inversChar() {
+function inversStr() {
   const argOutput = process.argv.slice(2);
   const myArgv = argOutput[0];
   let result = "";
 
   if (argOutput[1] || !myArgv ) {
-    return "only one argument is  autorized"
+    return "only one argument is autorized"
   } else {
     for (let i = (myArgv.length) -1  ; i >= 0 ; i--) {
         result += myArgv[i];
@@ -15,4 +15,4 @@ function inversChar() {
   return result;
 }
 
-console.log(inversChar());
+console.log(inversStr());
