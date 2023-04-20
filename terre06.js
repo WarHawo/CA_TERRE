f310bccd0acbe0cddd28dd0ec18f4c9c21e00510
hawo War <warhawo@gmail.com>
2023-04-20 13:44:27 +0000

    ex06 finit

---
+++ terre06.js
@@ -0,0 +1,18 @@
+/* Programme qui affiche l’inverse de la chaîne de caractères donnée en argument. */
+
+function inversChar() {
+  const argOutput = process.argv.slice(2);
+  const myArgv = argOutput[0];
+  let result = "";
+
+  if (argOutput[1] || !myArgv ) {
+    return "only one argument is  autorized"
+  } else {
+    for (let i = (myArgv.length) -1  ; i >= 0 ; i--) {
+        result += myArgv[i];
+      }
+  }
+  return result;
+}
+
+console.log(inversChar());
